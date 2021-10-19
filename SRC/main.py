5359cd02badcc274a9ff65a25dd9177b78bebf6a
Jyothish Kumar E.S <jyothishelanjimattom@gmail.com>
2021-10-19 08:52:10 +0530

    import time module to add because click doesn't work

--- SRC/main.py
+++ SRC/main.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
+import time
 
 from webdriver_manager.chrome import ChromeDriverManager
 browser=webdriver.Chrome(ChromeDriverManager().install())
 
 # browser = webdriver.Chrome(executable_path=(r'C:\Users\JyothishKumar\Chrome Driver\chromedriver'))
 browser.get('https://www.google.com/')
+time.sleep(2)
 
 #Automate searchkey
 search_input = browser.find_element_by_name('q')
 search_input.send_keys('Python tutorial')
+
+time.sleep(2)
 
 # <input class="gLFyf gsfi" jsaction="paste:puy29d;" maxlength="2048" name="q" type="text" aria-autocomplete="both" aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" autofocus="" role="combobox" spellcheck="false" title="Search" value="" aria-label="Search" data-ved="0ahUKEwiI6M6ru9XzAhVMpYsKHSCYCR0Q39UDCAQ">
 search_button = browser.find_element_by_css_selector('input[type="submit"]')
