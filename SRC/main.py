6ecb1ceea24d9157d9cc159bbe01a1880838b1fd
Jyothish Kumar E.S <jyothishelanjimattom@gmail.com>
2021-10-19 08:47:40 +0530

    added search button clickmethod

--- SRC/main.py
+++ SRC/main.py
@@ -11,3 +11,5 @@ search_input = browser.find_element_by_name('q')
 search_input.send_keys('Python tutorial')
 
 # <input class="gLFyf gsfi" jsaction="paste:puy29d;" maxlength="2048" name="q" type="text" aria-autocomplete="both" aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" autofocus="" role="combobox" spellcheck="false" title="Search" value="" aria-label="Search" data-ved="0ahUKEwiI6M6ru9XzAhVMpYsKHSCYCR0Q39UDCAQ">
+search_button = browser.find_element_by_css_selector('input[type="submit"]')
+search_button.click()
